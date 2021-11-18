b55da418b51c0792236d8c1c5701e34fe566a687
leandrodcs <eng.leandrodcs@gmail.com>
2021-11-18 15:55:21 -0300

    Sign-in route and function created

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,8 @@
+import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 import connection from '../database/database.js';
 import { userFactory } from '../factories/user.factory.js';
-import { validateUser } from '../validation/user.js';
+import { validateLogin, validateUser } from '../validation/user.js';
 
 async function signUp(req, res) {
     try {
@@ -22,6 +24,41 @@ async function signUp(req, res) {
     }
 }
 
+async function signIn(req, res) {
+    const {
+        email,
+        password,
+    } = req.body;
+
+    try {
+        if (validateLogin(req.body)) {
+            return res.sendStatus(400);
+        }
+
+        const users = await connection.query('SELECT * FROM users WHERE email = $1', [email]);
+        const user = users.rows[0];
+
+        if (!user) return res.sendStatus(401);
+
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+        if (!isPasswordCorrect) return res.sendStatus(401);
+
+        const token = uuid();
+        await connection.query('INSERT INTO sessions (user_id, token) VALUES ($1, $2)', [user.id, token]);
+
+        res.status(200).send({
+            name: user.name,
+            token,
+        });
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
 export {
     signUp,
+    signIn,
 };
