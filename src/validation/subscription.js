d96e75f84c1f8a2ba13225d5cd078f92f2cc2136
leandrodcs <eng.leandrodcs@gmail.com>
2021-11-21 01:46:44 -0300

    Changing the acceptable delivery dates

--- src/validation/subscription.js
+++ src/validation/subscription.js
@@ -8,7 +8,7 @@ function validateSubscription(sub) {
         zipCode: joi.string().length(9).regex(/^[0-9]{5}-[0-9]{3}$/).required(),
         city: joi.string().min(1).required(),
         stateId: joi.number().min(1).max(27).required(),
-        deliveryDate: joi.string().min(1).equal('Segunda', 'Quarta', 'Sexta', '01', '10', '20').required(),
+        deliveryDate: joi.string().min(1).equal('Segunda', 'Quarta', 'Sexta', '1', '10', '20').required(),
     }).unknown();
     return schema.validate(sub).error;
 }
